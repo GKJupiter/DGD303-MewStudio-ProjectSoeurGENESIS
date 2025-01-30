7abd258f8f2c336edd712b96e93179d281d9ff85
GKJupiter <godkingjupiter+github@gmail.com>
2025-01-30 22:46:47 +0300

    tp added

--- Assets/Scripts/TeleportationZone.cs
+++ Assets/Scripts/TeleportationZone.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class TeleportationZone : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Transform teleportDestination;  // Işınlanacak nokta
+    public string playerTag = "Player";    // Oyuncu tag'ini kontrol et
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        // Eğer çarpışan nesne oyuncuysa
+        if (other.CompareTag(playerTag))
+        {
+            // Oyuncuyu ışınla
+            other.transform.position = teleportDestination.position;
+        }
     }
 }
